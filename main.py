4a57462313d21ed3b61a558e53fec9ccf4166d43
Max van Rooijen <max.van.rooijen@knab.nl>
2019-03-27 18:44:18 +0100

    Fixed port

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ import os
 # BROKER_PORT: 1883
 ###
 
-rl = ar.EightChanRelay(os.environ.get('RELAY_IP'), 1234, 8)
+rl = ar.EightChanRelay(os.environ.get('RELAY_IP'), int(os.environ.get('RELAY_PORT')), 8)
 mqttc = mqtt.Client()
 topic = os.environ.get('BASE_TOPIC')
 broker_address = os.environ.get('BROKER_ADDRESS')
+broker_portno = int(os.environ.get('BROKER_PORT'))
 
 def on_connect(client, userdata, rc):
     print("Connected with result code "+str(rc))
@@ -38,5 +39,5 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect(broker_address)
+client.connect(broker_address, broker_portno)
 client.loop_forever()
